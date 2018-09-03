a82e2cf13dc83faaffa7cfd0b9cf4a3cb3c08d92
Yaron M. Minsky <yminsky@gmail.com>
2018-09-03 16:54:05 -0400

    added some docs, exposed [release] function

--- src/notty_async.mli
+++ src/notty_async.mli
@@ -18,6 +18,14 @@ module Term : sig
   val image : t -> Notty.image -> unit Deferred.t
   val cursor : t -> (int * int) option -> unit Deferred.t
   val size : t -> int * int
+
+  (** Release the terminal, restoring it to a state where ordinary I/O
+     can be performed. *)
+  val release : t -> unit Deferred.t
+
+  (** This pipe will automatically be shut down once [release] is
+     called, and closing this pipe will asynchronous trigger [release]
+     to be called. *)
   val events :
     t -> [ Notty.Unescape.event | `Resize of (int * int) ] Pipe.Reader.t
 end
